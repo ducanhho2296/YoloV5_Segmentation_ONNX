aa6309a097f3625e9b1d2defe6581c7538b8cadb
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-04 07:48:05 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -28,3 +28,4 @@ def xyxy2xywh(x):
     # Convert nx4 boxes from [x1, y1, x2, y2] to [x, y, w, h] where xy1=top-left, xy2=bottom-right
     y = np.copy(x)
     y[:, 0] = (x[:, 0] + x[:, 2]) / 2  # x center
+    y[:, 1] = (x[:, 1] + x[:, 3]) / 2  # y center
