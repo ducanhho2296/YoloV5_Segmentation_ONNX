24fa2844b8e394cfd49f81dc2f6dd0af4621e22a
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-05 09:25:49 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -51,3 +51,4 @@ def nms(dets, scores, thresh):
         xx1 = np.maximum(x1[i], x1[other_box_ids])
         yy1 = np.maximum(y1[i], y1[other_box_ids])
         xx2 = np.minimum(x2[i], x2[other_box_ids])
+        yy2 = np.minimum(y2[i], y2[other_box_ids])
