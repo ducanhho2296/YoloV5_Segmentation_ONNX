8d081d724713c8c81d10054acd87ffbd620d550d
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-06 09:00:28 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -75,3 +75,4 @@ def non_max_suppression(prediction, conf_thres=0.5, iou_thres=0.45, max_det=1000
 
     xc = prediction[..., 4] > conf_thres  # candidate
     # Settings
+    min_wh, max_wh = 2, 7680  # (pixels) minimum and maximum box width and height
