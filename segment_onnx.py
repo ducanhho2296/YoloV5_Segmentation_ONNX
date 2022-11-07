e4400e70434eafaa7163fb9781d8a7031e15740b
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-07 16:31:07 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -99,3 +99,4 @@ def non_max_suppression(prediction, conf_thres=0.5, iou_thres=0.45, max_det=1000
         conf = np.amax(x[:, 5:mi], axis=1, keepdims=True)
         j = np.argmax(x[:, 5:mi], axis=1).reshape(conf.shape)
         x = np.concatenate((box, conf, j.astype(float), mask), axis=1)[conf.flatten() > conf_thres]
+
