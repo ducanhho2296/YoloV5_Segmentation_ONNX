81689498915895444b5f9587816885d88dc29a07
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-07 16:31:11 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -104,3 +104,4 @@ def non_max_suppression(prediction, conf_thres=0.5, iou_thres=0.45, max_det=1000
         n = x.shape[0]  # number of boxes
         if not n:  # no boxes
             continue
+        elif n > max_nms:  # excess boxes
