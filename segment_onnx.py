87a84a6284784d9c0fa2b2aedfa5cefb26530ba5
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-08 11:03:45 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -130,3 +130,4 @@ def crop_mask(masks, boxes):
 
     n, h, w = masks.shape
     x1, y1, x2, y2 = np.split(boxes[:, :, None], 4, 1)  # x1 shape(1,1,n)
+    r = np.arange(w, device=masks.device, dtype=x1.dtype)[None, None, :]  # rows shape(1,w,1)
