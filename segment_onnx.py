93552af558fb6e8ed88a602d92c6f473ec3702fe
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-10 11:16:52 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -166,3 +166,4 @@ def process_mask(protos, masks_in, bboxes, shape, upsample=False):
 def scale_boxes(img1_shape, boxes, img0_shape, ratio_pad=None):
     # Rescale boxes (xyxy) from img1_shape to img0_shape
     if ratio_pad is None:  # calculate from img0_shape
+        gain = min(img1_shape[0] / img0_shape[0], img1_shape[1] / img0_shape[1])  # gain  = old / new
