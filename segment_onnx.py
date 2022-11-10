7632314a1c8e471deefb469e996ef0b2bf9d1c5b
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-10 11:16:57 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -167,3 +167,4 @@ def scale_boxes(img1_shape, boxes, img0_shape, ratio_pad=None):
     # Rescale boxes (xyxy) from img1_shape to img0_shape
     if ratio_pad is None:  # calculate from img0_shape
         gain = min(img1_shape[0] / img0_shape[0], img1_shape[1] / img0_shape[1])  # gain  = old / new
+        pad = (img1_shape[1] - img0_shape[1] * gain) / 2, (img1_shape[0] - img0_shape[0] * gain) / 2  # wh padding
