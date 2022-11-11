414308e8656801c4e9bf72afdd4014df7ba26d6c
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-11 20:21:55 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -189,3 +189,4 @@ def clip_boxes(boxes, shape):
     boxes[:, [0, 2]] = boxes[:, [0, 2]].clip(0, shape[1])  # x1, x2
     boxes[:, [1, 3]] = boxes[:, [1, 3]].clip(0, shape[0])  # y1, y2
 
+def is_ascii(s=''):
