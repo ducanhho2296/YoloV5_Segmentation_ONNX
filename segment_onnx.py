f28c6970b5a90a9579375aa34d3cdc8cb83110b3
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-11 20:22:00 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -193,3 +193,4 @@ def is_ascii(s=''):
 # Is string composed of all ASCII (no UTF) characters? (note str().isascii() introduced in python 3.7)
     s = str(s)  # convert list, tuple, None, etc. to str
     return len(s.encode().decode('ascii', 'ignore')) == len(s)
+
