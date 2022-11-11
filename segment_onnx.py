cceb6a9a6ac6c7454c5b1f96bf404eb62d00a5f7
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-11 20:22:17 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -213,3 +213,4 @@ def scale_image(im1_shape, masks, im0_shape, ratio_pad=None):
         raise ValueError(f'"len of masks shape" should be 2 or 3, but got {len(masks.shape)}')
     masks = masks[top:bottom, left:right]
     # masks = masks.permute(2, 0, 1).contiguous()
+    # masks = F.interpolate(masks[None], im0_shape[:2], mode='bilinear', align_corners=False)[0]
