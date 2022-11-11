b0d1491b4be619406f55b43d8cc826c632b53b20
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-11 20:22:20 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -217,3 +217,4 @@ def scale_image(im1_shape, masks, im0_shape, ratio_pad=None):
     # masks = masks.permute(1, 2, 0).contiguous()
     masks = cv2.resize(masks, (im0_shape[1], im0_shape[0]))
 
+    if len(masks.shape) == 2:
