012dfe138b7596002e1304ce4b80a476bc41b3d8
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-12 18:17:00 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -225,6 +225,7 @@ def letterbox(im, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleF
     # Resize and pad image while meeting stride-multiple constraints
     shape = im.shape[:2]  # current shape [height, width]
     if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
 
 
 
