5baa3106ce5d7338b5e8b891238b770fd0e8cfb7
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-12 18:17:17 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -250,3 +250,4 @@ def letterbox(im, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleF
         im = cv2.resize(im, new_unpad, interpolation=cv2.INTER_LINEAR)
     top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
     left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
