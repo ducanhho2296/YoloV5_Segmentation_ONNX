72473faf76bde04b58622d90a8bf2d2713e505e7
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-12 18:17:08 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -237,4 +237,5 @@ def letterbox(im, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleF
     new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
     dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
     if auto:  # minimum rectangle
+        dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
 
