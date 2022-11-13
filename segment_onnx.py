d55ed3494345dda03037c1959c0eea85a2d8a49a
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-13 09:54:42 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -267,3 +267,4 @@ def save_one_box(xyxy, im, file=Path('im.jpg'), gain=1.02, pad=10, square=False,
         file.parent.mkdir(parents=True, exist_ok=True)  # make directory
         f = str(increment_path(file).with_suffix('.jpg'))
         # cv2.imwrite(f, crop)  # save BGR, https://github.com/ultralytics/yolov5/issues/7007 chroma subsampling issue
+        Image.fromarray(crop[..., ::-1]).save(f, quality=95, subsampling=0)  # save RGB
