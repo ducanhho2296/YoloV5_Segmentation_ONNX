3dc7246f2c9c6483515e97ae9fb3d9326538e7c3
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-13 09:54:36 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -259,3 +259,4 @@ def save_one_box(xyxy, im, file=Path('im.jpg'), gain=1.02, pad=10, square=False,
     b = xyxy2xywh(xyxy)  # boxes
     if square:
         b[:, 2:] = b[:, 2:].max(1)[0].unsqueeze(1)  # attempt rectangle to square
+    b[:, 2:] = b[:, 2:] * gain + pad  # box wh * gain + pad
