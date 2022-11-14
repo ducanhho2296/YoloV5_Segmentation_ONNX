c613391c517b28dbe23d2a29fd01461be29eb236
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-14 11:02:59 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -270,3 +270,4 @@ def save_one_box(xyxy, im, file=Path('im.jpg'), gain=1.02, pad=10, square=False,
         Image.fromarray(crop[..., ::-1]).save(f, quality=95, subsampling=0)  # save RGB
     return crop
 
+class LoadImages:
