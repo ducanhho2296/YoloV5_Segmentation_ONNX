712a326205f36f2f24947bf3e51e813477ebba95
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-15 10:57:00 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -327,3 +327,4 @@ class LoadImages:
     def _cv2_rotate(self, im):
         # Rotate a cv2 video manually
         if self.orientation == 0:
+            return cv2.rotate(im, cv2.ROTATE_90_CLOCKWISE)
