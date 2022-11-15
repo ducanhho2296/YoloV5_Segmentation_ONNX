1403a807bef19ba5c1940069537088d07f2be3cd
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-15 10:56:52 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -322,3 +322,4 @@ class LoadImages:
             im = im.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
             im = np.ascontiguousarray(im)  # contiguous
 
+        return path, im, im0
