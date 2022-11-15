8ae74017ebed607594aaf4f05d109851455c274b
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-15 10:57:03 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -331,3 +331,4 @@ class LoadImages:
         elif self.orientation == 180:
             return cv2.rotate(im, cv2.ROTATE_90_COUNTERCLOCKWISE)
         elif self.orientation == 90:
+            return cv2.rotate(im, cv2.ROTATE_180)
