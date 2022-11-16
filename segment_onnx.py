8c3db436ebc7cebba2f746ba640616af4b8b3375
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-16 10:00:51 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -363,3 +363,4 @@ class Annotator:
     def __init__(self, im, line_width=None, font_size=None, font='Arial.ttf', pil=False, example='abc'):
         assert im.data.contiguous, 'Image not contiguous. Apply np.ascontiguousarray(im) to Annotator() input images.'
         non_ascii = not is_ascii(example)  # non-latin labels, i.e. asian, arabic, cyrillic
+        self.pil = pil or non_ascii
