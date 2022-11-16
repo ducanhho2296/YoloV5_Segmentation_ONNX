c2abdfc96bb00db09a084d2b9fddcf65eaceff92
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-16 10:00:33 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -347,4 +347,5 @@ class Colors:
         self.n = len(self.palette)
 
     def __call__(self, i, bgr=False):
+        c = self.palette[int(i) % self.n]
         return (c[2], c[1], c[0]) if bgr else c
