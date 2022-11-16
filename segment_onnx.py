e5239fefdfbb10ff4f26470140dcdf690faf1fbc
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-16 10:00:56 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -369,3 +369,4 @@ class Annotator:
         #     self.draw = ImageDraw.Draw(self.im)
         # else:  # use cv2
         self.im = im
+        self.lw = line_width or max(round(sum(im.shape) / 2 * 0.003), 2)  # line width
