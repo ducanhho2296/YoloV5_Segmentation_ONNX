a0489384c6d9fa848832307e2899a866631ef707
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-17 18:39:03 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -387,3 +387,4 @@ class Annotator:
         #         # self.draw.text((box[0], box[1]), label, fill=txt_color, font=self.font, anchor='ls')  # for PIL>8.0
         #         self.draw.text((box[0], box[1] - h if outside else box[1]), label, fill=txt_color, font=self.font)
         # else:  # cv2
+            p1, p2 = (int(box[0]), int(box[1])), (int(box[2]), int(box[3]))
