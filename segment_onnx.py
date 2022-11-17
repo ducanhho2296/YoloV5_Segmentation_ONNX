cac0d5157f2feb1d1fbb47fbc68c2d504fc4e1f9
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-17 18:39:05 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -390,3 +390,4 @@ class Annotator:
             p1, p2 = (int(box[0]), int(box[1])), (int(box[2]), int(box[3]))
             cv2.rectangle(self.im, p1, p2, color, thickness=self.lw, lineType=cv2.LINE_AA)
             if label:
+                w, h = cv2.getTextSize(label, 0, fontScale=self.lw / 3, thickness=tf)[0]  # text width, height
