511b7a718d3cbae75eda77f8b9c841bd1f947af4
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-17 18:38:52 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -375,3 +375,4 @@ class Annotator:
     def box_label(self, box, label='', color=(128, 128, 128), txt_color=(255, 255, 255)):
         # Add one xyxy box to image with label
         # if self.pil or not is_ascii(label):
+        #     self.draw.rectangle(box, width=self.lw, outline=color)  # box
