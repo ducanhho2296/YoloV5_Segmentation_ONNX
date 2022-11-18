f84d554ae70b90a8e4ae67514c795044be506c67
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-18 12:39:51 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -408,3 +408,4 @@ class Annotator:
     def masks(self, masks, colors, im_gpu=None, alpha=0.5):
         """Plot masks at once.
         Args:
+            masks (tensor): predicted masks on cuda, shape: [n, h, w]
