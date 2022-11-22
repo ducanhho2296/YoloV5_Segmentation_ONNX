51dff7a89a8160739248ae8bbc00bced90d22027
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-22 10:54:45 +0100

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -445,3 +445,4 @@ class Annotator:
         #     im_gpu = im_gpu.flip(dims=[0])  # flip channel
         #     im_gpu = im_gpu.permute(1, 2, 0).contiguous()  # shape(h,w,3)
         #     im_gpu = im_gpu * inv_alph_masks[-1] + mcs
+        #     im_mask = (im_gpu * 255).byte().cpu().numpy()
