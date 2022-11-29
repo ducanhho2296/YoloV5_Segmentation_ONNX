8211b6f0216ceb08fe9730e418a618296a76a7b6
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-29 08:32:08 +0700

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -466,3 +466,4 @@ class Annotator:
         # Update self.im from a numpy array
         self.im = im if isinstance(im, Image.Image) else Image.fromarray(im)
         self.draw = ImageDraw.Draw(self.im)
+
