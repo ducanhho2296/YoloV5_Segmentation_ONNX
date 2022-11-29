1556f75083b651f5ceed816d1b65649bac1368f3
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-29 08:31:56 +0700

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -453,3 +453,4 @@ class Annotator:
 
     def rectangle(self, xy, fill=None, outline=None, width=1):
         # Add rectangle to image (PIL-only)
+        self.draw.rectangle(xy, fill, outline, width)
