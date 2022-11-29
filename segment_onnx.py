98dbd28f2c8192316a97deec968c6fedf8024289
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-11-29 15:43:43 +0700

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -501,3 +501,4 @@ for path, img, im0s in dataset:
     session = onnxruntime.InferenceSession(model, providers=providers)
     input_name = session.get_inputs()[0].name
     output_name1 = session.get_outputs()[0].name
+    output_name2 = session.get_outputs()[1].name
