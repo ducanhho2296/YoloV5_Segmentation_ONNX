4b92af0519773e4b8ca75822884536be0226d65e
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-12-01 07:10:34 +0700

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -523,3 +523,4 @@ for path, img, im0s in dataset:
             masks = process_mask(proto_mask[i], det[:, 6:], det[:, :4], img.shape[2:], upsample=True)  # HWC
             det[:, :4] = scale_boxes(img.shape[2:], det[:, :4], img.shape).round()
 
+
