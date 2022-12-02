53c12d1a8e8b72091b8cbcb22245dca25552e668
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-12-02 10:09:37 +0700

    Update segment_onnx.py

--- segment_onnx.py
+++ segment_onnx.py
@@ -543,3 +543,4 @@ for path, img, im0s in dataset:
 
         cv2.namedWindow(str(i), cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)  # allow window resize (Linux)
         cv2.resizeWindow(str(i), img1.shape[1], img1.shape[0])
+        cv2.imshow("demo.jpg", img1)
