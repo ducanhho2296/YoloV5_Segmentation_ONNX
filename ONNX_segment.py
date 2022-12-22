29abfb6869084fdb5760f4387fed46be11df5915
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-12-22 08:13:12 +0700

    Update ONNX_segment.py

--- ONNX_segment.py
+++ ONNX_segment.py
@@ -30,3 +30,4 @@ class Segmentator:
         y[:, 0] = (x[:, 0] + x[:, 2]) / 2  # x center
         y[:, 1] = (x[:, 1] + x[:, 3]) / 2  # y center
         y[:, 2] = x[:, 2] - x[:, 0]  # width
+        y[:, 3] = x[:, 3] - x[:, 1]  # height
