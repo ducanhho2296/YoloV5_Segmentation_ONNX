28479bb841373121f05cfb668b798c7f3b2b5359
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-12-24 09:44:24 +0700

    Update ONNX_segment.py

--- ONNX_segment.py
+++ ONNX_segment.py
@@ -77,3 +77,4 @@ class Segmentator:
         # Settings
         min_wh, max_wh = 2, 7680  # (pixels) minimum and maximum box width and height
         max_nms = 30000  # maximum number of boxes into torchvision.ops.nms()
+
