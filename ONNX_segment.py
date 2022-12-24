096322af9d04dbef4fbdc6334ea2e5f4bb0187bb
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-12-24 09:44:29 +0700

    Update ONNX_segment.py

--- ONNX_segment.py
+++ ONNX_segment.py
@@ -83,3 +83,4 @@ class Segmentator:
 
         for xi, x in enumerate(prediction):  # image index, image inference
             # Apply constraints
+            # x[((x[..., 2:4] < min_wh) | (x[..., 2:4] > max_wh)).any(1), 4] = 0  # width-height
