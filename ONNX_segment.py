d44d64bb4334a247fd49db77582b44506fe31984
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-12-26 12:57:17 +0700

    Update ONNX_segment.py

--- ONNX_segment.py
+++ ONNX_segment.py
@@ -96,3 +96,4 @@ class Segmentator:
             box = self.xywh2xyxy(x[:, :4])  #Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] (line 912/general.py)
             mask = x[:, mi:]
             # Detections matrix nx6 (xyxy, conf, cls)
+            conf = np.amax(x[:, 5:mi], axis=1, keepdims=True)
