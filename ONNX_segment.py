9d8d6b930fda739935fcc6e94295123288335916
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-12-26 12:57:20 +0700

    Update ONNX_segment.py

--- ONNX_segment.py
+++ ONNX_segment.py
@@ -100,3 +100,4 @@ class Segmentator:
             j = np.argmax(x[:, 5:mi], axis=1).reshape(conf.shape)
             x = np.concatenate((box, conf, j.astype(float), mask), axis=1)[conf.flatten() > conf_thres]
 
+            # Check shape
