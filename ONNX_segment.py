2b1c6cf45a0af29ae69b432a8df9a17fdec84f32
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-01-02 13:49:15 +0700

    Update ONNX_segment.py

--- ONNX_segment.py
+++ ONNX_segment.py
@@ -172,3 +172,4 @@ class Segmentator:
         # Rescale boxes (xyxy) from img1_shape to img0_shape
         if ratio_pad is None:  # calculate from img0_shape
             gain = min(img1_shape[0] / img0_shape[0], img1_shape[1] / img0_shape[1])  # gain  = old / new
+            pad = (img1_shape[1] - img0_shape[1] * gain) / 2, (img1_shape[0] - img0_shape[0] * gain) / 2  # wh padding
