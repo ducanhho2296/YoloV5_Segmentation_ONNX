3f5de2465a03bc7bd7b956775d223c3049abaa9d
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-01-03 13:14:26 +0700

    Update ONNX_segment.py

--- ONNX_segment.py
+++ ONNX_segment.py
@@ -198,3 +198,4 @@ class Segmentator:
     # Is string composed of all ASCII (no UTF) characters? (note str().isascii() introduced in python 3.7)
         s = str(s)  # convert list, tuple, None, etc. to str
         return len(s.encode().decode('ascii', 'ignore')) == len(s)
+
