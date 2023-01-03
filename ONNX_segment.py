500b82240dfccec04398d84dd68956ddf4ee6b92
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-01-03 13:14:44 +0700

    Update ONNX_segment.py

--- ONNX_segment.py
+++ ONNX_segment.py
@@ -219,3 +219,4 @@ class Segmentator:
         masks = masks[top:bottom, left:right]   
         # masks = masks.permute(2, 0, 1).contiguous()
         # masks = F.interpolate(masks[None], im0_shape[:2], mode='bilinear', align_corners=False)[0]
+        # masks = masks.permute(1, 2, 0).contiguous()
