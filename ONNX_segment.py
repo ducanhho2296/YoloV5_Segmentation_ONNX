942f382149116ed3e9feb89ff1e85569917cc559
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-01-03 13:14:35 +0700

    Update ONNX_segment.py

--- ONNX_segment.py
+++ ONNX_segment.py
@@ -210,3 +210,4 @@ class Segmentator:
             gain = min(im1_shape[0] / im0_shape[0], im1_shape[1] / im0_shape[1])  # gain  = old / new
             pad = (im1_shape[1] - im0_shape[1] * gain) / 2, (im1_shape[0] - im0_shape[0] * gain) / 2  # wh padding
         else:
+            pad = ratio_pad[1]
