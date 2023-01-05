eb5da64e7a7eca5d14e94fb40ea33b8b2bea11a0
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-01-05 15:45:15 +0700

    Update ONNX_segment.py

--- ONNX_segment.py
+++ ONNX_segment.py
@@ -234,3 +234,4 @@ class Segmentator:
 
         # Scale ratio (new / old)
         r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+        r1 = max(new_shape[0] /shape[0], new_shape[1] / shape[1])
