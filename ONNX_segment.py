c36dc2cd4d0d330d768f5b3485352710a789b41f
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-01-05 15:45:27 +0700

    Update ONNX_segment.py

--- ONNX_segment.py
+++ ONNX_segment.py
@@ -246,3 +246,4 @@ class Segmentator:
         #     dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
         if scaleFill:  # stretch
             dw, dh = 0.0, 0.0
+            new_unpad = (new_shape[1], new_shape[0])
