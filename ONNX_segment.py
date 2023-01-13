6dc3773afe064e2cf3726376ae127ea9d3023a3b
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-01-13 18:51:10 +0700

    Update ONNX_segment.py

--- ONNX_segment.py
+++ ONNX_segment.py
@@ -318,3 +318,4 @@ class Segmentator:
         if self.orientation == 0:
             return cv2.rotate(im, cv2.ROTATE_90_CLOCKWISE)
         elif self.orientation == 180:
+            return cv2.rotate(im, cv2.ROTATE_90_COUNTERCLOCKWISE)
