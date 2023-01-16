171c55e90cfc6ccc4299effe16bc1df59a03adaa
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-01-16 07:55:20 +0100

    Update ONNX_segment.py

--- ONNX_segment.py
+++ ONNX_segment.py
@@ -334,3 +334,4 @@ class Colors:
                 '2C99A8', '00C2FF', '344593', '6473FF', '0018EC', '8438FF', '520085', 'CB38FF', 'FF95C8', 'FF37C7')
         self.palette = [self.hex2rgb(f'#{c}') for c in hexs]
         self.n = len(self.palette)
+
