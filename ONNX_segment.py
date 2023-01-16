f8d1d7db51e817d1d4a36f34354f5377d3440dec
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-01-16 07:55:17 +0100

    Update ONNX_segment.py

--- ONNX_segment.py
+++ ONNX_segment.py
@@ -331,3 +331,4 @@ class Colors:
     def __init__(self):
         # hex = matplotlib.colors.TABLEAU_COLORS.values()
         hexs = ('FF3838', 'FF9D97', 'FF701F', 'FFB21D', 'CFD231', '48F90A', '92CC17', '3DDB86', '1A9334', '00D4BB',
+                '2C99A8', '00C2FF', '344593', '6473FF', '0018EC', '8438FF', '520085', 'CB38FF', 'FF95C8', 'FF37C7')
