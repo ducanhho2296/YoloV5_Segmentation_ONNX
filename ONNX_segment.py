667052bea6432c16ba8120cb7e6c5d5d75618e3a
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-01-17 10:42:20 +0100

    Update ONNX_segment.py

--- ONNX_segment.py
+++ ONNX_segment.py
@@ -356,3 +356,4 @@ class Annotator(Segmentator):
         self.lw = line_width or max(round(sum(im.shape) / 2 * 0.003), 2)  # line width
 
 
+    def box_label(self, box, label='', color=(128, 128, 128), txt_color=(255, 255, 255)):
