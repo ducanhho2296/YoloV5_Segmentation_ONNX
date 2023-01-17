a0f207d5c3f7da9262e24105d84fc4a1c0e5c38e
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-01-17 10:42:24 +0100

    Update ONNX_segment.py

--- ONNX_segment.py
+++ ONNX_segment.py
@@ -360,3 +360,4 @@ class Annotator(Segmentator):
         p1, p2 = (int(box[0]), int(box[1])), (int(box[2]), int(box[3]))
         cv2.rectangle(self.im, p1, p2, color, thickness=self.lw, lineType=cv2.LINE_AA)
         if label:
+            tf = max(self.lw - 1, 1)  # font thickness
