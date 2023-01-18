5ec33273d8afec7e7254e0cb35a33ac177b8725f
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-01-18 11:49:58 +0100

    Update ONNX_segment.py

--- ONNX_segment.py
+++ ONNX_segment.py
@@ -382,3 +382,4 @@ class Annotator(Segmentator):
             colors (List[List[Int]]): colors for predicted masks, [[r, g, b] * n]
             im_gpu (tensor): img is in cuda, shape: [3, h, w], range: [0, 1]
             alpha (float): mask transparency: 0.0 fully transparent, 1.0 opaque
+        """
