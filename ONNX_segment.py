85389c24beb03f6d1603f272dd0bc387fb17a89f
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-02-15 11:12:00 +0100

    Update ONNX_segment.py

--- ONNX_segment.py
+++ ONNX_segment.py
@@ -558,3 +558,4 @@ if __name__ == "__main__":
             img = img[None]
 
         pred_onnx = session.run([output_name1], {input_name: img})
+        proto_onnx = session.run([output_name2], {input_name: img})
