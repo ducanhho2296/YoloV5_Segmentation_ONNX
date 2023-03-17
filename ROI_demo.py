7d38def05d0a6ce8ca923125e51083b6796d2cae
novoai <92146886+ducanhho2296@users.noreply.github.com>
2023-03-17 10:59:26 +0100

    Update ROI_demo.py

--- ROI_demo.py
+++ ROI_demo.py
@@ -15,3 +15,13 @@ roi = img[y:y+h, x:x+w]
 # Calculate the average brightness of the ROI
 gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
 avg_brightness = cv2.mean(gray_roi)[0]
+
+# Adjust the exposure of the entire image based on the average brightness of the ROI
+exposure_compensation = int(round((128 - avg_brightness) / 16))
+adjusted_img = cv2.add(img, exposure_compensation)
+
+# Display the original image and the adjusted image side by side
+cv2.imshow('Original Image', img)
+cv2.imshow('Adjusted Image', adjusted_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
