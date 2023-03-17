7ed8b36ee256e0bce8a310a64271566fb5dc69c6
novoai <92146886+ducanhho2296@users.noreply.github.com>
2023-03-17 11:04:20 +0100

    update

--- ROI_demo.py
+++ ROI_demo.py
@@ -1,8 +1,7 @@
 import cv2
 
 # Load the image
-img = cv2.imread("C:/Users/NovoAIUser/Documents/GitHub/FastAPI-Real-time-data-streaming/demo.jpg")
-
+img = cv2.imread("img_path")
 cv2.namedWindow('Select ROI', cv2.WINDOW_NORMAL)
 cv2.imshow('Select ROI', img)
 rect = cv2.selectROI('Select ROI', img, False)
@@ -10,6 +9,7 @@ cv2.destroyWindow('Select ROI')
 
 # Extract the ROI from the image
 x, y, w, h = rect
+
 roi = img[y:y+h, x:x+w]
 
 # Calculate the average brightness of the ROI
