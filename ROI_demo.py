5340bcb6cd3089caa69cd0b76e3de546c1f7d12d
novoai <92146886+ducanhho2296@users.noreply.github.com>
2023-03-17 10:59:18 +0100

    Update ROI_demo.py

--- ROI_demo.py
+++ ROI_demo.py
@@ -4,3 +4,4 @@ import cv2
 img = cv2.imread("C:/Users/NovoAIUser/Documents/GitHub/FastAPI-Real-time-data-streaming/demo.jpg")
 
 cv2.namedWindow('Select ROI', cv2.WINDOW_NORMAL)
+cv2.imshow('Select ROI', img)
